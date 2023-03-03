953e2a177d5a27984d73bd2a1145e7dee2669f3b
Henry Read <henrya2@126.com>
2023-03-03 15:37:43 +0800

    test code

--- src/point_gen.cpp
+++ src/point_gen.cpp
@@ -1,15 +1,22 @@
 #include <darts/common.h>
 #include <darts/sampling.h>
+#include <darts/factory.h>
+#include <darts/sampler.h>
 
 int main(int argc, char **argv)
 {
     fmt::print("x,y,z\n");
     //fmt::print("0,0,0\n");
-    pcg32          rng;
-    for (int i = 0; i < 500; ++i)
+    shared_ptr<Sampler> sampler = DartsFactory<Sampler>::create({{"type", "cmj"}, {"samples", 400}});
+    sampler->seed(2, 2); 
+    sampler->start_pixel(0, 0);
+    for (int i = 0; i < (int)sampler->sample_count(); ++i)
     {
-        Vec3f p = sample_hemisphere_cosine_power(20, Vec2f{rng.nextFloat(), rng.nextFloat()});
+        //Vec3f p = Vec3f(sample_disk(sampler->next2f()), 0.f);
+        Vec3f p = sample_hemisphere(sampler->next2f());
         fmt::print("{},{},{}\n", p.x, p.y, p.z);
+
+        sampler->advance();
     }
 
     return 0;
